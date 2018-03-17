2e42f3b40d9014c738265be802439ebbc8fc9a0c
Landon Carr <landoncarr@Landons-MacBook-Pro.local>
2018-03-17 14:37:28 -0600

    Phase 1 Add Button Via Code to MainVC

--- WindowShopper/WindowShopper/Views/CurrenyTxtField.swift
+++ WindowShopper/WindowShopper/Views/CurrenyTxtField.swift
@@ -8,14 +8,21 @@
 
 import UIKit
 
+
+@IBDesignable
 class CurrenyTxtField: UITextField {
 
-  
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
     
     override func awakeFromNib() {
         
-        super.awakeFromNib()
         
+        super.awakeFromNib()
+        customizeView()
+    }
+        func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -33,5 +40,6 @@ class CurrenyTxtField: UITextField {
         
       
     }
+
 
 }
