a77b0ee1391002031986b697097934341566976c
Landon Carr <landoncarr@Landons-MacBook-Pro.local>
2018-03-17 14:10:09 -0600

    Phase 0 Window Shopper

---
+++ WindowShopper/WindowShopper/Controller/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WindowShopper
+//
+//  Created by Landon Carr on 3/17/18.
+//  Copyright © 2018 Landon Carr. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
