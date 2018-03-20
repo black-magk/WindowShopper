9a750dd4cdee5294c85ce2e325b183aec89dbdea
Landon Carr <landoncarr@Landons-MacBook-Pro.local>
2018-03-20 13:38:11 -0600

    WindowShopper App Class Complete

--- WindowShopper/WindowShopper/Controller/MainVC.swift
+++ WindowShopper/WindowShopper/Controller/MainVC.swift
@@ -14,6 +14,7 @@ class MainVC: UIViewController {
     @IBOutlet weak var hoursLbl: UILabel!
     @IBOutlet weak var resultLbl: UILabel!
     
+
     
     
     override func viewDidLoad() {
--- WindowShopper/WindowShopper/Views/CurrenyTxtField.swift
+++ WindowShopper/WindowShopper/Views/CurrenyTxtField.swift
@@ -11,6 +11,25 @@ import UIKit
 
 @IBDesignable
 class CurrenyTxtField: UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.9746669229, green: 1, blue: 0.9511624895, alpha: 0.7802600599)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+        
+        
+        
+    }
 
     override func prepareForInterfaceBuilder() {
         customizeView()
@@ -27,6 +46,7 @@ class CurrenyTxtField: UITextField {
         layer.cornerRadius = 5.0
         textAlignment = .center
         
+         clipsToBounds = true
         
         if let p = placeholder {
             
